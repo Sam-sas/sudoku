56276ce6587ab324c0bb68886957230e15ee4139
samantha <sashepar@gmail.com>
2025-05-16 12:23:17 -0400

    create rules page and make header more responsive

--- src/atoms/Headings.jsx
+++ src/atoms/Headings.jsx
@@ -1,4 +1,4 @@
-const Heading = ({ size = "h1", title = "Heading", fontSize = "text-6xl" }) => {
+const Heading = ({ size = "h1", title = "Heading", fontSize = "text-4xl md:text-6xl" }) => {
   const Tag = size;
   return (
     <Tag className={"capitalize font-pencil m-4 " + fontSize}>{title}</Tag>
